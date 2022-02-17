ecdce72e74c6a82fc94b2f68b049f4e97802f084
Komal1234144 <tolambia.komal5@gmail.com>
2022-02-17 13:04:27 +0530

    commit-3

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,18 @@ import { initializeApp} from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
-  appId: process.env.REACT_APP_FIREBASE_API_ID
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
+  // appId: process.env.REACT_APP_FIREBASE_API_ID
+  apiKey: "AIzaSyDhQUBB0cU9NVSxyACBn23K_OpHm0dznAs",
+  authDomain: "clone-d789a.firebaseapp.com",
+  projectId: "clone-d789a",
+  storageBucket: "clone-d789a.appspot.com",
+  messagingSenderId: "1003227083371",
+  appId: "1:1003227083371:web:da559b0c224a7bac11372e"
 };
 
 
