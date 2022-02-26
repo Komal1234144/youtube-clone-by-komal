fb7f4a5ecacf143698911bb9ad804d91e7b09b5e
Komal1234144 <tolambia.komal5@gmail.com>
2022-02-26 10:11:46 +0530

    chnages

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const request = axios.create({
  baseURL : "https://youtube.googleapis.com/youtube/v3/",
  params : {
-     key : process.env.REACT_APP_YT_API_KEY
+     key : "AIzaSyC07ecfH6-X8Hp1arNJuS1dyv66y_p32us"//process.env.REACT_APP_YT_API_KEY
  }
 })
--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,18 @@ import { initializeApp} from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
-  appId: process.env.REACT_APP_FIREBASE_API_ID
-  // apiKey: "AIzaSyDrJhvK4MmPdafiW8V2loBKE0G5WqKMlDg",
-  // authDomain: "clone-73815.firebaseapp.com",
-  // projectId: "clone-73815",
-  // storageBucket: "clone-73815.appspot.com",
-  // messagingSenderId: "71210730635",
-  // appId: "1:71210730635:web:7b575083e7b703284b008e"
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
+  // appId: process.env.REACT_APP_FIREBASE_API_ID
+  apiKey: "AIzaSyC07ecfH6-X8Hp1arNJuS1dyv66y_p32us",
+  authDomain: "clone-95057.firebaseapp.com",
+  projectId: "clone-95057",
+  storageBucket: "clone-95057.appspot.com",
+  messagingSenderId: "1032251112493",
+  appId: "1:1032251112493:web:f5f35a5389cfeca2c24ca4"  
 };
 
 
